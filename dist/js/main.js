f1fcd0860ff653330fcccfb247d0f2f7d4c16eda
Siarhei Drozd <drozd1586229@gmail.com>
2019-10-11 19:42:06 +0200

    added: responsive navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,7 @@
 $("span .slider .round").click(function () {
     $(".main-background").toggleClass("main-background-night");
 });
+
+$('.navbar__hamburger').click(() => {
+    $('.navbar__right-side').toggleClass('navbar__right-side--visible');
+});
